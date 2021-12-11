553c3b22e75ce331d0e167ce80814733906591cb
cquigley2020 <61098258+cquigley2020@users.noreply.github.com>
2021-12-10 22:00:53 -0600

    Update hardware.dart

--- GeneralGoodsStore/lib/Models/hardware.dart
+++ GeneralGoodsStore/lib/Models/hardware.dart
@@ -2,20 +2,30 @@ class Hardware {
   final String id;
   final String hardwareBrand;
   final String hardwareModel;
+  final String hardwarePrice;
+  final String hardwareQuantity;
   final String hardwareYear;
   final DateTime hardwareExpiration;
 
-  Hardware._(this.id, this.hardwareBrand, this.hardwareModel, this.hardwareYear,
+  Hardware._(
+      this.id,
+      this.hardwareBrand,
+      this.hardwareModel,
+      this.hardwarePrice,
+      this.hardwareQuantity,
+      this.hardwareYear,
       this.hardwareExpiration);
 
   factory Hardware.fromJson(Map json) {
     final id = json[''].replaceAll('ObjectId("', '').replaceAll('"', '');
     final hardwareBrand = json['hardwareBrand'];
     final hardwareModel = json['hardwareModel'];
+    final hardwarePrice = json['hardwarePrice'];
+    final hardwareQuantity = json['hardwareQuantity'];
     final hardwareYear = json['hardwareYear'];
     final hardwareExpiration = json['hardwareExpiration'];
 
-    return Hardware._(
-        id, hardwareBrand, hardwareModel, hardwareYear, hardwareExpiration);
+    return Hardware._(id, hardwareBrand, hardwareModel, hardwarePrice,
+        hardwareQuantity, hardwareYear, hardwareExpiration);
   }
 }
