796fb5e09ad65683466a8b5715901a187e6bed06
cquigley2020 <61098258+cquigley2020@users.noreply.github.com>
2021-12-10 22:07:53 -0600

    Update homegoods.dart

--- GeneralGoodsStore/lib/Models/homegoods.dart
+++ GeneralGoodsStore/lib/Models/homegoods.dart
@@ -2,20 +2,30 @@ class Homegoods {
   final String id;
   final String homegoodsBrand;
   final String homegoodsModel;
+  final String homegoodsPrice;
+  final String homegoodsQuantity;
   final String homegoodsYear;
   final DateTime homegoodsExpiration;
 
-  Homegoods._(this.id, this.homegoodsBrand, this.homegoodsModel,
-      this.homegoodsYear, this.homegoodsExpiration);
+  Homegoods._(
+      this.id,
+      this.homegoodsBrand,
+      this.homegoodsModel,
+      this.homegoodsPrice,
+      this.homegoodsQuantity,
+      this.homegoodsYear,
+      this.homegoodsExpiration);
 
   factory Homegoods.fromJson(Map json) {
     final id = json[''].replaceAll('ObjectId("', '').replaceAll('"', '');
     final homegoodsBrand = json['homegoodsBrand'];
     final homegoodsModel = json['homegoodsModel'];
+    final homegoodsPrice = json['homegoodsPrice'];
+    final homegoodsQuantity = json['homegoodsQuantity'];
     final homegoodsYear = json['homegoodsYear'];
     final homegoodsExpiration = json['homegoodsExpiration'];
 
-    return Homegoods._(
-        id, homegoodsBrand, homegoodsModel, homegoodsYear, homegoodsExpiration);
+    return Homegoods._(id, homegoodsBrand, homegoodsModel, homegoodsPrice,
+        homegoodsQuantity, homegoodsYear, homegoodsExpiration);
   }
 }
