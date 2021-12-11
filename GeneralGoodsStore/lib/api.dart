2e97dda91b5575a515ea3f3f3c0e625d75a3026e
cquigley2020 <61098258+cquigley2020@users.noreply.github.com>
2021-12-10 22:10:24 -0600

    Create api.dart

---
+++ GeneralGoodsStore/lib/api.dart
@@ -0,0 +1,6 @@
+import 'package:dio/dio.dart';
+import './Models/appliances.dart';
+import './Models/electronics.dart';
+import './Models/hardware.dart';
+import './Models/homegoods.dart';
+import './Models/perishables.dart';
