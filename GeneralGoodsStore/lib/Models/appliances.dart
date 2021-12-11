0d58216968a2fb014f9e858f0d6ab9095dcb9df6
cquigley2020 <61098258+cquigley2020@users.noreply.github.com>
2021-12-10 21:59:47 -0600

    Update appliances.dart

--- GeneralGoodsStore/lib/Models/appliances.dart
+++ GeneralGoodsStore/lib/Models/appliances.dart
@@ -2,20 +2,30 @@ class Appliance {
   final String id;
   final String applianceBrand;
   final String applianceModel;
+  final String appliancePrice;
+  final String applianceQuantity;
   final String applianceYear;
   final DateTime applianceExpiration;
 
-  Appliance._(this.id, this.applianceBrand, this.applianceModel,
-      this.applianceYear, this.applianceExpiration);
+  Appliance._(
+      this.id,
+      this.applianceBrand,
+      this.applianceModel,
+      this.appliancePrice,
+      this.applianceQuantity,
+      this.applianceYear,
+      this.applianceExpiration);
 
   factory Appliance.fromJson(Map json) {
     final id = json[''].replaceAll('ObjectId("', '').replaceAll('"', '');
     final applianceBrand = json['applianceBrand'];
     final applianceModel = json['applianceModel'];
+    final appliancePrice = json['appliancePrice'];
+    final applianceQuantity = json['applianceQuantity'];
     final applianceYear = json['applianceYear'];
     final applianceExpiration = json['applianceExpiration'];
 
-    return Appliance._(
-        id, applianceBrand, applianceModel, applianceYear, applianceExpiration);
+    return Appliance._(id, applianceBrand, applianceModel, appliancePrice,
+        applianceQuantity, applianceYear, applianceExpiration);
   }
 }
