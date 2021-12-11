3da5a16eca2f6f4aaccf0753c97ff3e1182c213a
cquigley2020 <61098258+cquigley2020@users.noreply.github.com>
2021-12-10 22:09:19 -0600

    Update perishables.dart

--- GeneralGoodsStore/lib/Models/perishables.dart
+++ GeneralGoodsStore/lib/Models/perishables.dart
@@ -3,19 +3,35 @@ class Perishables {
   final String perishableBrand;
   final String perishableName;
   final DateTime perishableStockDate;
+  final String perishablePrice;
+  final String perishableQuantity;
   final DateTime perishablesExpiration;
 
-  Perishables._(this.id, this.perishableBrand, this.perishableName,
-      this.perishableStockDate, this.perishablesExpiration);
+  Perishables._(
+      this.id,
+      this.perishableBrand,
+      this.perishableName,
+      this.perishableStockDate,
+      this.perishablePrice,
+      this.perishableQuantity,
+      this.perishablesExpiration);
 
   factory Perishables.fromJson(Map json) {
     final id = json[''].replaceAll('ObjectId("', '').replaceAll('"', '');
     final perishableBrand = json['perishableBrand'];
     final perishableName = json['perishableName'];
     final perishableStockDate = json['perishableStockDate'];
+    final perishablePrice = json['perishablePrice'];
+    final perishableQuantity = json['perishableQuantity'];
     final perishablesExpiration = json['perishablesExpiration'];
 
-    return Perishables._(id, perishableBrand, perishableName,
-        perishableStockDate, perishablesExpiration);
+    return Perishables._(
+        id,
+        perishableBrand,
+        perishableName,
+        perishableStockDate,
+        perishablePrice,
+        perishableQuantity,
+        perishablesExpiration);
   }
 }
