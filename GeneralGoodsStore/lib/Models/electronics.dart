ae7759cb99794a09485437d4e18cb1d7d9a6499b
cquigley2020 <61098258+cquigley2020@users.noreply.github.com>
2021-12-10 22:00:17 -0600

    Update electronics.dart

--- GeneralGoodsStore/lib/Models/electronics.dart
+++ GeneralGoodsStore/lib/Models/electronics.dart
@@ -2,20 +2,36 @@ class Electronics {
   final String id;
   final String electronicsBrand;
   final String electronicsModel;
+  final String electronicsPrice;
+  final String electronicsQuantity;
   final String electronicsYear;
   final DateTime electronicsExpiration;
 
-  Electronics._(this.id, this.electronicsBrand, this.electronicsModel,
-      this.electronicsYear, this.electronicsExpiration);
+  Electronics._(
+      this.id,
+      this.electronicsBrand,
+      this.electronicsModel,
+      this.electronicsPrice,
+      this.electronicsQuantity,
+      this.electronicsYear,
+      this.electronicsExpiration);
 
   factory Electronics.fromJson(Map json) {
     final id = json[''].replaceAll('ObjectId("', '').replaceAll('"', '');
     final electronicsBrand = json['electronicsBrand'];
     final electronicsModel = json['electronicsModel'];
+    final electronicsPrice = json['electronicsPrice'];
+    final electronicsQuantity = json['electronicsQuantity'];
     final electronicsYear = json['electronicsYear'];
     final electronicsExpiration = json['electronicsExpiration'];
 
-    return Electronics._(id, electronicsBrand, electronicsModel,
-        electronicsYear, electronicsExpiration);
+    return Electronics._(
+        id,
+        electronicsBrand,
+        electronicsModel,
+        electronicsPrice,
+        electronicsQuantity,
+        electronicsYear,
+        electronicsExpiration);
   }
 }
